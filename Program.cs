450e5e3aa9ac064cc9fcec517d591c7271a90905
Afsar Nadheem C <afsar17052002@gmail.com>
2024-04-20 14:14:40 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace UpcommingLarge
+{
+    public class Program
+    {
+        static int [] list = { 3,5,2,6,1,4,2,6,3 } ;
+        static int large = Int32.MaxValue;
+        public static void Main(string[] args)
+        {
+            for (int i = 0;i<list.Count() ; i++){
+                if (checkLarge(i) == large){
+                    Console.WriteLine(list[i] + " -> ");
+                }
+                else{
+                    Console.WriteLine(list[i] + " -> " + checkLarge(i));
+                }
+            }
+        }
+
+        public static int checkLarge (int index)
+        {
+            int max = large;
+            for (int i = index+1; i < list.Count(); i++){
+                if (list[i] < max && list[i] > list[index]) max = list[i];
+            }
+            return max;
+        }
+    }
+}
